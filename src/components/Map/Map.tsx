d3c6b275d38c6d8b245dbbe400e162351f285083
Kate Grutter <kategrutter@gmail.com>
2023-05-10 21:20:00 -0400

    gameboard divs set up in Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,46 @@
-import './Map.css';
+import "./Map.css";
 
 export function Map() {
+  return (
+    <div>
+      <div className="gameboard">
+        <div className="gameboard-row" data-row="1" id="row-1">
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+        </div>
 
-    return(
-        <div></div>
-    )
+        <div className="gameboard-row" data-row="2" id="row-2">
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+        </div>
+        <div className="gameboard-row" data-row="3" id="row-3">
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+        </div>
+        <div className="gameboard-row" data-row="4" id="row-4">
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+        </div>
+        <div className="gameboard-row" data-row="5" id="row-5">
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
