6d20a09291ae8abe0dfec5bb0ad21280a534b409
Desiree Cushing <desireecushing@gmail.com>
2023-05-11 19:41:48 -0400

    added start of useState for addimprovement

--- src/components/Map/MapComponents/AddImprovementDialog.tsx
+++ src/components/Map/MapComponents/AddImprovementDialog.tsx
@@ -3,8 +3,14 @@ import { useState } from "react";
 import { Improvement } from "../../../models/Improvement";
 
 
-export function AddImprovementDialog() {
+export function AddImprovementDialog(props: { onAddImprovement: (newImprovement: Improvement) => void }) {
     const [open, setOpen] = useState(false);
+
+    const [people, setPeople] = useState('');
+    const [grain, setGrain] = useState('');
+    const [sheep, setSheep] = useState('');
+    const [lumber, setLumber] = useState('');
+    const [water, setWater] = useState('');
 
     const handleOpen = () => {
         setOpen(!open);
@@ -12,27 +18,42 @@ export function AddImprovementDialog() {
 
     return (
         <div className="dropdown">
-            <button onClick={handleOpen}>Dropdown</button>
-            {open ? (
-                <ul className="menu">
-                    <li className="menu-item">
-                        <button>People</button>
-                    </li>
-                    <li className="menu-item">
-                        <button>Grain</button>
-                    </li>
-                    <li className="menu-item">
-                        <button>Sheep</button>
-                    </li>
-                    <li className="menu-item">
-                        <button>Lumber</button>
-                    </li>
-                    <li className="menu-item">
-                        <button>Water</button>
-                    </li>
 
-                </ul>
-            ) : null}
+            <form onSubmit={e => {
+                e.preventDefault();
+
+                const newImprovement: Improvement = {
+                    type: type,
+                    level: +level,
+                    benefit: benefit,
+                    cost: cost
+                }
+
+                props.onAddImprovement(newImprovement);
+            }}>
+
+                <button onClick={handleOpen}>Dropdown</button>
+                {open ? (
+                    <ul className="menu">
+                        <li className="menu-item">
+                            <button>People</button>
+                        </li>
+                        <li className="menu-item">
+                            <button>Grain</button>
+                        </li>
+                        <li className="menu-item">
+                            <button>Sheep</button>
+                        </li>
+                        <li className="menu-item">
+                            <button>Lumber</button>
+                        </li>
+                        <li className="menu-item">
+                            <button>Water</button>
+                        </li>
+
+                    </ul>
+                ) : null}
+            </form>
         </div>
     );
 }
