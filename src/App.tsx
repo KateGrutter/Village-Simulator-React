fdc16f121888d2fa24589fdc2bdb21782782855d
jgreener6 <justinmgreen06@gmail.com>
2023-05-10 21:54:52 -0400

    Updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Map } from './components/Map/Map';
+import { ResourceView } from './components/ResourcesView/ResourcesView';
 
 function App() {
   return (
     <div className="App">
+      <Map></Map>
+      <ResourceView></ResourceView>
       
     </div>
   );
