73f71ccab333d033afdfd8bc6c1949be3cde977a
jgreener6 <justinmgreen06@gmail.com>
2023-05-12 12:24:43 -0400

    working to add onclick to have dialog appears

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,30 +1,101 @@
 
-
-
-
-
-
-
-
-
-
-
-
 import { useState } from "react";
 import { Improvement } from "../../models/Improvement";
 import "./Map.css";
 import { MapProps } from "./MapComponents/MapProps";
+import { AddImprovementDialog } from "./MapComponents/AddImprovementDialog";
+import { House } from "../../store/ImprovementsCost";
 
-
-
-export function Map({onTileClick}: MapProps) {
-
-  const [improvement, setImprovement] = useState<Improvement[]>([]);
+export function Map({ onTileClick }: MapProps) {
+  const [improvements, setImprovements] = useState<Improvement[][]>([]);
+  const [showAddImprovementDialog, setShowAddImprovementDialog] = useState(false);
+  const [selectedTileIndex, setSelectedTileIndex] = useState<number | null>(null);
 
   const handleTileClick = (tileIndex: number) => {
-    console.log("Tile clicked:", tileIndex);
-    onTileClick(tileIndex);
+    setSelectedTileIndex(tileIndex);
+    setShowAddImprovementDialog(true);
   };
+
+  const addImprovement = (newImprovement: Improvement) => {
+    const updatedImprovements = [...improvements];
+    if (selectedTileIndex !== null) {
+      if (!updatedImprovements[selectedTileIndex]) {
+        updatedImprovements[selectedTileIndex] = [];
+      }
+      updatedImprovements[selectedTileIndex].push(newImprovement);
+      setImprovements(updatedImprovements);
+    }
+  };
+
+  return (
+    <div>
+      <div className="gameboard">
+      <div className="tile" onClick={() => onTileClick(0)}>
+            {improvements[0] && <ImprovementComponent key ={0} improvement={improvements[0]} />}</div>
+            <div className="tile" onClick={() => onTileClick(1)}>
+            {improvements[0] && <ImprovementComponent key ={1} improvement={improvements[0]} />}</div>
+            <div className="tile" onClick={() => onTileClick(2)}>
+            {improvements[0] && <ImprovementComponent key ={2} improvement={improvements[0]} />}</div>
+            <div className="tile" onClick={() => onTileClick(3)}>
+            {improvements[0] && <ImprovementComponent key ={0} improvement={improvements[0]} />}</div>
+            <div className="tile" onClick={() => onTileClick(4)}>
+            {improvements[0] && <ImprovementComponent key ={4} improvement={improvements[0]} />}</div>
+      </div>
+
+      {showAddImprovementDialog && (
+        <AddImprovementDialog
+          onClose={() => setShowAddImprovementDialog(false)}
+          onAddImprovement={addImprovement}
+          selectedTileIndex={selectedTileIndex !== null ? selectedTileIndex : 0}
+          // Other props you may need to pass
+        />
+      )}
+    </div>
+  );
+}
+
+interface ImprovementComponentProps {
+  improvement: Improvement;
+}
+
+function ImprovementComponent({ improvement }: ImprovementComponentProps) {
+  // Render the improvement component based on the provided improvement object
+  return <div>{/* Improvement component JSX */}</div>;
+}
+
+
+
+
+
+
+/*
+import { useState } from "react";
+import { Improvement } from "../../models/Improvement";
+import "./Map.css";
+import { MapProps } from "./MapComponents/MapProps";
+import { AddImprovementDialog } from "./MapComponents/AddImprovementDialog";
+
+
+export function Map({ onTileClick }: MapProps) {
+  const [improvement, setImprovements] = useState<Improvement[][]>([]);
+  const [showAddImprovementDialog, setShowAddImprovementDialog] = useState(false);
+  const [selectedTileIndex, setSelectedTileIndex] = useState<number | null>(null);
+
+  const handleTileClick = (tileIndex: number) => {
+    setSelectedTileIndex(tileIndex);
+    setShowAddImprovementDialog(true);
+  };
+
+  const addImprovement = (newImprovement: Improvement) => {
+    const updatedImprovements = [...improvement];
+    if (!updatedImprovements[selectedTileIndex]) {
+      updatedImprovements[selectedTileIndex] = [];
+    }
+    updatedImprovements[selectedTileIndex].push(newImprovement);
+    setImprovements(updatedImprovements);
+  };
+
+
 
   return (
     <div>
@@ -32,62 +103,62 @@ export function Map({onTileClick}: MapProps) {
         <div className="gameboard-row" data-row="1" id="row-1">
           <div className="tile" onClick={() => onTileClick(0)}>
             {improvement[0] && <ImprovementComponent key ={0} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(1)}>
             {improvement[0] && <ImprovementComponent key ={1} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(2)}>
             {improvement[0] && <ImprovementComponent key ={2} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
-            {improvement[0] && <ImprovementComponent key ={3} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(3)}>
+            {improvement[0] && <ImprovementComponent key ={0} improvement={improvement[0]} />}</div>
+            <div className="tile" onClick={() => onTileClick(4)}>
             {improvement[0] && <ImprovementComponent key ={4} improvement={improvement[0]} />}</div>
         </div>
 
         <div className="gameboard-row" data-row="2" id="row-2">
-        <div className="tile" onClick={() => onTileClick(0)}>
+        <div className="tile" onClick={() => onTileClick(5)}>
             {improvement[0] && <ImprovementComponent key ={0} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(6)}>
             {improvement[0] && <ImprovementComponent key ={1} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(7)}>
             {improvement[0] && <ImprovementComponent key ={2} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(8)}>
             {improvement[0] && <ImprovementComponent key ={3} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(9)}>
             {improvement[0] && <ImprovementComponent key ={4} improvement={improvement[0]} />}</div>
         </div>
         <div className="gameboard-row" data-row="3" id="row-3">
-        <div className="tile" onClick={() => onTileClick(0)}>
+        <div className="tile" onClick={() => onTileClick(10)}>
             {improvement[0] && <ImprovementComponent key ={0} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(11)}>
             {improvement[0] && <ImprovementComponent key ={1} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(12)}>
             {improvement[0] && <ImprovementComponent key ={2} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(13)}>
             {improvement[0] && <ImprovementComponent key ={3} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(14)}>
             {improvement[0] && <ImprovementComponent key ={4} improvement={improvement[0]} />}</div>
         </div>
         <div className="gameboard-row" data-row="4" id="row-4">
-        <div className="tile" onClick={() => onTileClick(0)}>
+        <div className="tile" onClick={() => onTileClick(15)}>
             {improvement[0] && <ImprovementComponent key ={0} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(16)}>
             {improvement[0] && <ImprovementComponent key ={1} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(17)}>
             {improvement[0] && <ImprovementComponent key ={2} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(18)}>
             {improvement[0] && <ImprovementComponent key ={3} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(19)}>
             {improvement[0] && <ImprovementComponent key ={4} improvement={improvement[0]} />}</div>
         </div>
         <div className="gameboard-row" data-row="5" id="row-5">
-        <div className="tile" onClick={() => onTileClick(0)}>
+        <div className="tile" onClick={() => onTileClick(20)}>
             {improvement[0] && <ImprovementComponent key ={0} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(21)}>
             {improvement[0] && <ImprovementComponent key ={1} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(22)}>
             {improvement[0] && <ImprovementComponent key ={2} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(23)}>
             {improvement[0] && <ImprovementComponent key ={3} improvement={improvement[0]} />}</div>
-            <div className="tile" onClick={() => onTileClick(0)}>
+            <div className="tile" onClick={() => onTileClick(24)}>
             {improvement[0] && <ImprovementComponent key ={4} improvement={improvement[0]} />}</div>
         </div>
       </div>
@@ -102,5 +173,5 @@ interface ImprovementComponentProps {
 
 function ImprovementComponent({ improvement }: ImprovementComponentProps) {
   // Render the improvement component based on the provided improvement object
-  return <div>{/* Improvement component JSX */}</div>;
-} ;
+  return <div>{/* Improvement component JSX *//*}</div>;
+} ;*/
--- src/components/Map/MapComponents/AddImprovementDialog.tsx
+++ src/components/Map/MapComponents/AddImprovementDialog.tsx
@@ -1,4 +1,91 @@
-import { Map } from "../Map";
+import { useState } from "react";
+import { Improvement } from "../../../models/Improvement";
+
+ interface AddImprovementDialogProps {
+  onClose: () => void;
+  onAddImprovement: (newImprovement: Improvement) => void;
+  selectedTileIndex: number;
+}
+
+export function AddImprovementDialog({ onClose, onAddImprovement, selectedTileIndex }: AddImprovementDialogProps) {
+  const [open, setOpen] = useState(false);
+  const [selectedImprovement, setSelectedImprovement] = useState('');
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleItemClick = (improvementType: string) => {
+    setSelectedImprovement(improvementType);
+    setOpen(false);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newImprovement: Improvement = {
+      type: selectedImprovement,
+      level: 1,
+      resourceProduced: {
+        type: selectedImprovement,
+        amount: 1
+        },
+        cost: [
+            {
+                type: selectedImprovement,
+                amount: 1
+            }
+        ]
+      // Add other properties as needed
+    };
+
+    onAddImprovement(newImprovement);
+    onClose();
+  };
+
+  return (
+    <div className="dropdown">
+      <form onSubmit={handleSubmit}>
+        <button onClick={handleOpen}>Dropdown</button>
+        {open && (
+          <ul className="menu">
+            <li className="menu-item">
+              <button onClick={() => handleItemClick("People")}>People</button>
+            </li>
+            <li className="menu-item">
+              <button onClick={() => handleItemClick("Grain")}>Grain</button>
+            </li>
+            <li className="menu-item">
+              <button onClick={() => handleItemClick("Sheep")}>Sheep</button>
+            </li>
+            <li className="menu-item">
+              <button onClick={() => handleItemClick("Lumber")}>Lumber</button>
+            </li>
+            <li className="menu-item">
+              <button onClick={() => handleItemClick("Water")}>Water</button>
+            </li>
+          </ul>
+        )}
+
+        <button onClick={onClose}>Cancel</button>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Map } from "../Map";
 import { useState } from "react";
 import { Improvement } from "../../../models/Improvement";
 
@@ -61,4 +148,4 @@ export function AddImprovementDialog() {
             </form>
         </div>
     );
-}
+}*/
