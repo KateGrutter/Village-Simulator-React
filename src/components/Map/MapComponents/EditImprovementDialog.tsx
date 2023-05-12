657c2a8d246182d06b01124e12d1f98fa03a7c25
Kate Grutter <kategrutter@gmail.com>
2023-05-12 08:10:17 -0400

    work done on EditImprovementDialog and component added into the tile component

--- src/components/Map/MapComponents/EditImprovementDialog.tsx
+++ src/components/Map/MapComponents/EditImprovementDialog.tsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import { Improvement } from "../../../models/Improvement";
 
 
-export function EditImprovementDialog() {
+export function EditImprovementDialog(props: {improvement: Improvement, onClose: (improvement: Improvement) => void, onUpgrade: (improvement: Improvement) => void, onDowngrade: (improvement: Improvement) => void, onRemove: (improvement: Improvement) => void}) {
     return (
         <div>
-            <form>
+            <div className="improvement-info">
+                <p>Type {props.improvement.type}</p>
+                <p>Level {props.improvement.level}</p>
+                <p>Benefit </p>
+                <div> Cost {
+                props.improvement.cost.map(newImprovement => props.improvement.cost)}</div>
+            </div>
+            <div className="buttons-container">
                 <button>Close</button>
                 <button>Upgrade</button>
                 <button>Downgrade</button>
                 <button>Remove</button>
-            </form>
+            </div>
         </div>
     )
 }
--- src/components/Map/MapComponents/Tile.tsx
+++ src/components/Map/MapComponents/Tile.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { Improvement } from "../../../models/Improvement";
+import { EditImprovementDialog } from "./EditImprovementDialog";
 
 
-export function Tile() {
+export function Tile(props: {improvement: Improvement, onClose: (improvement: Improvement) => void, onUpgrade: (improvement: Improvement) => void, onDowngrade: (improvement: Improvement) => void, onRemove: (improvement: Improvement) => void}) {
     return (
-        <div></div>
+        <div>
+            <EditImprovementDialog improvement={props.improvement} onUpgrade={props.onUpgrade} onDowngrade={props.onDowngrade} onClose={props.onClose} onRemove={props.onRemove}></EditImprovementDialog>
+        </div>
     )
 }
+
+// {props.improvement.type} {props.improvement.level}{props.improvement.resourceProduced.amount}{props.improvement.cost.map(newCost => props.improvement.cost)}
