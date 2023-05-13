02e74badffdde7b9a4e4baa0486e2ec693b8e343
Desiree Cushing <desireecushing@gmail.com>
2023-05-13 16:59:18 -0400

    updating addimprovement

--- src/components/Map/MapComponents/AddImprovementDialog.tsx
+++ src/components/Map/MapComponents/AddImprovementDialog.tsx
@@ -2,36 +2,67 @@ import { useState } from "react";
 import { Improvement } from "../../../models/Improvement";
 import { ImprovementCosts } from "../../../store/ImprovementsCost";
 
+interface AddImprovementDialogProps {
+  onClose: (improvement?: Improvement) => void;
+  onAdd: (improvement: Improvement) => void;
+  improvement: Improvement | undefined;
+}
 
-//  interface AddImprovementDialogProps {
-//   onClose: () => void;
-//   onAddImprovement: (newImprovement: Improvement) => void;
-//   selectedTileIndex: number;
-// }
+export function AddImprovementDialog(props: AddImprovementDialogProps) {
+  const [selectedImprovement, setSelectedImprovement] = useState<Improvement | undefined>(props.improvement);
 
-//add selectedTileIndex back callback props when ready ( directly down below)
-// do we want to also call the interface : AddImprovementDialogProps?
+  const handleCancel = () => {
+    setSelectedImprovement(undefined);
+    props.onClose();
+  };
 
-//(props: { improvement: Improvement, onClose: (improvement: Improvement) => void, onAddImprovement: (improvement: Improvement) => void }
-
-export function AddImprovementDialog(props: { improvement: Improvement, onClose: (improvement: Improvement) => void, onAdd: (improvement: Improvement) => void }) {
-
-  const [selectedImprovement, setSelectedImprovement] = useState<Improvement | undefined>(undefined); //sets improvement chosen from dropdown menu
-
-
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (selectedImprovement) {
+      props.onAdd(selectedImprovement);
+    }
+    setSelectedImprovement(undefined);
+    props.onClose();
+  };
   return (
-    <div className="dropdown">
-      <form>
-        <select onChange={e => setSelectedImprovement(ImprovementCosts.find(improvement => e.target.value === improvement.type))} name="cars" id="cars">
-          {ImprovementCosts.map(improvement => <option value={improvement.type}>{improvement.type}</option>)}
+    <div className="add-improvement-dialog">
+      <form onSubmit={handleAdd}>
+        <select
+          onChange={(e) =>
+            setSelectedImprovement(
+              ImprovementCosts.find(
+                (improvement) => e.target.value === improvement.type
+              )
+            )
+          }
+          name="cars"
+          id="cars"
+        >
+          <option disabled selected>
+            Please select a type
+          </option>
+          {ImprovementCosts.map((improvement) => (
+            <option key={improvement.type} value={improvement.type}>
+              {improvement.type}
+            </option>
+          ))}
         </select>
-        <div>Benefit {selectedImprovement ? selectedImprovement.resourceProduced.amount : ''} {selectedImprovement ? selectedImprovement.resourceProduced.type : ''} </div>
-        <div>Cost {selectedImprovement?.cost.map((cost, index) => (
-          <li key={index}>
-            {cost.amount} {cost.type}
-          </li>))}
+        <div>
+          Benefit{" "}
+          {selectedImprovement ? selectedImprovement.resourceProduced.amount : ""}{" "}
+          {selectedImprovement ? selectedImprovement.resourceProduced.type : ""}{" "}
         </div>
-        <button>Cancel</button>
+        <div>
+          Cost{" "}
+          {selectedImprovement?.cost.map((cost, index) => (
+            <li key={index}>
+              {cost.amount} {cost.type}
+            </li>
+          ))}
+        </div>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
         <button type="submit">Add</button>
       </form>
     </div>
@@ -40,4 +71,3 @@ export function AddImprovementDialog(props: { improvement: Improvement, onClose:
 
 
 //onClick={onAdd(selectedImprovement)} <-----SUBMIT BUTTON IDEAS
-// {selectedImprovement.cost.map( cost => cost.amount )}
